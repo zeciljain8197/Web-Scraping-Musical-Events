82f6ee61e4b2724bdbab17391db5c707353f5973
Zecil Jain <zecil.jain@gmail.com>
2022-11-18 20:11:32 +0530

    Adding final project files

---
+++ main.py
@@ -0,0 +1,72 @@
+import requests
+import selectorlib
+import smtplib, ssl
+import os
+import time
+import sqlite3
+
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+
+connection = sqlite3.connect("data.db")
+
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url)
+    source = response.text
+    return source
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "zeciljain07@gmail.com"
+    password = "nryquykmkslxpuey"
+
+    receiver = "zeciljain07@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+    print("Email was sent!")
+
+
+def store(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
+
+
+def read(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    return rows
+
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        if extracted != "No upcoming tours":
+            row = read(extracted)
+            if not row:
+                store(extracted)
+                send_email(message="Hey, new event was found!")
+        time.sleep(2)
